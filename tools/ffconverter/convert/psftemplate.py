cabf1db9726ee9e8389368c26175a7b0a4670c38
Brent Gregersen <gregerse@deshawresearch.com>
2020-07-23 10:59:58 -0400

    fix impropers and cmap generation. also allow subselection

--- tools/ffconverter/convert/psftemplate.py
+++ tools/ffconverter/convert/psftemplate.py
@@ -5,7 +5,9 @@ def parse_args():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument("psf", type=os.path.realpath, help="charmm psf file")
     parser.add_argument(
-        "ffdir", type=os.path.realpath, help="Output template file into a viparr forcefied"
+        "ffdir",
+        type=os.path.realpath,
+        help="Output template file into a viparr forcefied",
     )
     parser.add_argument(
         "-f",
@@ -14,10 +16,17 @@ def parse_args():
         action="store_true",
         help="Fuse all residues into a single template",
     )
+    parser.add_argument(
+        "-s",
+        "--selection",
+        default="all",
+        type=str,
+        help="msys atom selection of atoms in the psf to convert into templates",
+    )
     return parser.parse_args()
 
 
-def psftemplate(psfmol, fuse):
+def psftemplate(psfmol, fuse, selection):
     import viparr
     from viparr import ffconverter
 
@@ -25,18 +34,25 @@ def psftemplate(psfmol, fuse):
         [0, 0, 1.0], [0, 0, 1.0], "charmm"
     )
 
+    mysel = selection
+    if mysel != "all" and "same" not in mysel:
+        mysel = "same residue as "+mysel
+    only = psfmol.select(mysel)
+
     nbtable = psfmol.table("nonbonded")
 
-    if (fuse):
-        residues = psfmol.updateFragids()
-        for frag in residues:
-            name = []
-            for res in sorted({(a.residue.id, a.residue) for a in frag}):
-                name.append(res[1].name)
-            frag[0].residue.name = "_".join(name)
-    else:
-        residues = [r.atoms for r in psfmol.residues]
+    reslist = psfmol.updateFragids() if fuse else psfmol.residues
+    residues = []
+    for r in reslist:
+        atoms = [a for a in r if a in only]
+        if len(atoms) == 0 : continue
+        residues.append(atoms)
 
+    if fuse:
+        for frag in residues:
+           frag[0].residue.name = "_".join(sorted({a.residue.name for a in frag}))
+
+    nbtable = psfmol.table("nonbonded")
     for ires, resatoms in enumerate(residues):
         tsys = viparr.TemplatedSystem()
         res = tsys.system.addResidue()
@@ -69,7 +85,7 @@ def psftemplate(psfmol, fuse):
         }
         for i, atom in enumerate(dangling):
             a = res.addAtom()
-            a.name = f"$i"
+            a.name = f"${i}"
             a.atomic_number = -1
             amap[atom] = a
 
@@ -81,17 +97,19 @@ def psftemplate(psfmol, fuse):
 
         if "improper_harm" in psfmol.table_names:
             for impr in psfmol.table("improper_harm").terms:
-                print([a.fullname for a in impr.atoms])
                 alist = [amap[a] for a in impr.atoms if a in amap]
-                if alist != 4 or sum([1 if a.atomic_number > 0 else 0 for a in alist]) <3:
+                asum = sum([1 if a.atomic_number > 0 else 0 for a in alist])
+                if len(alist) != 4 or asum < 3:
                     continue
                 tsys.addImproper(alist)
 
-        if "torsion_torsion_cmap" in psfmol.table_names:
-            for cmap in psfmol.table("torsion_torsion_cmap").terms:
+        if "torsiontorsion_cmap" in psfmol.table_names:
+            for cmap in psfmol.table("torsiontorsion_cmap").terms:
                 alist = [amap[a] for a in cmap.atoms if a in amap]
-                if alist != 8 or sum([1 if a.atomic_number > 0 else 0 for a in alist]) <7:
+                asum = sum([1 if a.atomic_number > 0 else 0 for a in alist])
+                if len(alist) != 8 or asum < 6:
                     continue
+                print(alist)
                 tsys.addCmap(alist)
 
         print(res.name, qsum)
@@ -106,5 +124,5 @@ def main():
     args = parse_args()
     mol = msys.Load(args.psf)
 
-    ff = psftemplate(mol, args.fuse)
+    ff = psftemplate(mol, args.fuse, args.selection)
     viparr.ExportForcefield(ff, args.ffdir)
